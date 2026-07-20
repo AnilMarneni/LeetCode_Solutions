3ef52710b29a8ab35fd0f1307afa370a15d04052
Marneni Anil Chiranjeeth <154552119+AnilMarneni@users.noreply.github.com>
2026-07-20 17:26:16 +0530

    Time: 51 ms (27.72%) | Memory: 84.9 MB (34.04%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        unordered_map<int, int> freq;
+        int left = 0;
+        int maxNum = 0;
+        for(int right = 0; right < n; right++){
+            freq[fruits[right]]++;
+            while(freq.size() > 2){
+                freq[fruits[left]]--;
+                if(freq[fruits[left]] == 0){
+                    freq.erase(fruits[left]);
+                }
+                left++;
+            }
+            maxNum = max(maxNum, right - left + 1);
+        }
+        return maxNum;
+    }
+};
